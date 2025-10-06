8ded35c07fed7710605f288fec8d71528d1e6bb9
@stawmbonald <Scott-F4@ulster.ac.uk>
2025-10-06 14:51:09 +0100

    Added properties to allow access to title, author, ISBN properties

--- LibraryApp2/Book.cs
+++ LibraryApp2/Book.cs
@@ -2,12 +2,28 @@
 
 namespace LibraryApp2
 {
-    internal class Book
+     class Book
     {
             
-        string Title;
-        string Author;
-        string ISBN;
+        private string title;
+        private string author;
+        private string isbn;
+
+        public string Title
+        {
+            get { return Title; }  
+            set { Title = value; }  
+        }
+        public string Author
+        {
+            get { return Author; }  
+            set { Author = value; }
+        }
+        public string ISBN
+        {
+            get { return ISBN; }  
+            set { ISBN = value; }
+        }
 
         public Book(string bookTitle, string bookAuthor, string bookISBN)
         {
