44a0987fe366375e9b910c287da91e78e203ae2c
@stawmbonald <Scott-F4@ulster.ac.uk>
2025-10-06 15:19:05 +0100

    Added the person class

---
+++ LibraryApp2/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp2
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }  
+        public Person()
+        {
+            this.FirstName = "";
+            this.LastName = "";
+        }
+        public Person(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
